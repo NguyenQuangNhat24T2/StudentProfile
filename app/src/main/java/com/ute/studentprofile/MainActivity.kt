9715908b50d0ca0fef5865515ccca99641cc572b
Nguyen Quang Nhat <nguyenquangnhat200806@gmail.com>
2026-09-21 13:57:07 +0700

    "build: building -> build main activity logic v2"

--- app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -1,24 +1,19 @@
-package com.example.studentprofile
+package com.ute.studentprofile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.studentprofile.databinding.ActivityMainBinding
+import com.ute.studentprofile.databinding.ActivityMainBinding  // <-- Sửa dòng này, bỏ chữ "example" đi
 
 class MainActivity : AppCompatActivity() {
 
-    // Bước 1: Khai báo biến binding với lateinit var
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Bước 2: Nạp layout XML thông qua hàm inflate() của ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // Bước 3: Truyền root view vào setContentView thay vì dùng R.layout.activity_main
         setContentView(binding.root)
 
-        // Bước 4: Thao tác View trực tiếp thông qua biến binding (Không cần findViewById)
         binding.tvWelcome.text = "Chào mừng bạn đến với ViewBinding!"
     }
 }
